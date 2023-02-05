e4b464cffb0e6e89a067a9ce085ce9194a2414f0
HP <HP@DESKTOP-K1L6537>
2023-02-05 12:21:18 +0530

    committing amazon project

---
+++ src/com/Amazon/TestClass/TC003_Validate_url.java
@@ -0,0 +1,9 @@
+package com.Amazon.TestClass;
+
+public class TC003_Validate_url {
+
+	public static void main (String [] args)
+	{
+		System.out.println("Today is Holiday....Hurraayyyyyeeee");
+	}
+}
